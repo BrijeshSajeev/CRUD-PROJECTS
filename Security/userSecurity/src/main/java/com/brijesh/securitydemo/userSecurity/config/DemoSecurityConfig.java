ceeb5e5edb237307a7789fe963fa0220de7b6c48
BrijeshSajeev <brijesh00300@gmail.com>
2023-07-25 22:10:59 +0530

    feat:login with db

---
+++ Security/userSecurity/src/main/java/com/brijesh/securitydemo/userSecurity/config/DemoSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.brijesh.securitydemo.userSecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Controller;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//
+//        http.authorizeHttpRequests(configurer ->
+//                configurer
+//                        .requestMatchers("/").hasRole("USER")
+////                        .requestMatchers("/leaders/**").hasRole("MANAGER")
+//                        .anyRequest().authenticated()
+//
+//                );
+//            return http.build();
+//    }
+
+}
